917f591fa50f47d9a12998a7196f9397097831bb
Alexander <dunin.vit@gmail.com>
2018-04-22 18:09:10 +0300

    Update RatingRepository.java

--- src/main/java/com/example/ConspectSite/repository/RatingRepository.java
+++ src/main/java/com/example/ConspectSite/repository/RatingRepository.java
@@ -8,15 +8,7 @@ import java.util.List;
 
 public interface RatingRepository extends CrudRepository<Rating, Long> {
 
-   /* @Query(value = "SELECT rate FROM rating r INNER JOIN fanfiction_rates c ON r.id=c.rates_id WHERE r.user_account_id= :userId AND c.fanfiction_id= :fanfictionId",
-            nativeQuery = true)
-    Integer getUserRate(@Param("userId") Long userId, @Param("fanfictionId") Long fanfictionId);*/
-
    Rating getRatingByUserIdAndConspectId(long userId, long conspectId);
-
-   /* @Query(value = "SELECT AVG(rate) FROM rating r INNER JOIN fanfiction_rates c ON r.id = c.rates_id WHERE c.fanfiction_id = :fanfictionId",
-            nativeQuery = true)
-    Double getAverageRateByFanfictionId(@Param("fanfictionId") Long fanfictionId);*/
 
    Integer countAllByConspectId(long conspectId);
 
